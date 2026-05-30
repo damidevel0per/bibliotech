db287ac1218c3bf70f806b66f8552232bb3aa87a
damidevel0per <damidevtech@gmail.com>
2026-05-30 14:35:46 -0300

    feat: nueva funcionalidad para gargar un libro mediante el formulario

--- frontendbibliotech/src/pages/CargarLibroPage.jsx
+++ frontendbibliotech/src/pages/CargarLibroPage.jsx
@@ -6,6 +6,61 @@ import './CargarLibroPage.css';
 function CargarLibroPage() {
     const [portadaUrl, setPortadaUrl] = useState("");
 
+    const [libro, setLibro] = useState({
+        nombre: "",
+        autores: [],
+        categorias: [],
+        anioPublicacion: "",
+        paginas: "",
+        descripcion: "",
+        idioma: "",
+        portada: "",
+        linkPdf: "",
+        formato: "PDF"
+    });
+
+    const handleChange = (e) => {
+        setLibro({
+            ...libro,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const libroParaEnviar = {
+            ...libro,
+            autores: [libro.autores]
+        };
+        
+        console.log(libroParaEnviar);
+
+        try {
+            const response = await fetch("http://localhost:3001/libros", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(libroParaEnviar)
+            });
+
+            if (!response.ok) {
+                throw new Error("Error al guardar el libro");
+            }
+
+            const data = await response.json();
+
+            console.log("Libro guardado:", data);
+            alert("Libro cargado correctamente");
+        } catch (error) {
+            console.error(error);
+            alert("Ocurrió un error");
+        }
+    };
+
+
+
     return (<>
         <HeaderComponent>
         </HeaderComponent>
@@ -13,34 +68,78 @@ function CargarLibroPage() {
             <h2>Agrega un nuevo libro a la biblioteca</h2>
             <br />
             <div className="cargar-libro-form">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor="titulo">Título:</label>
-                        <input placeholder='Clean code' type="text" id="titulo" name="titulo" required />
+                        <label htmlFor="nombre">Título:</label>
+                        <input
+                            placeholder='Clean code'
+                            value={libro.nombre}
+                            onChange={handleChange}
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="autor">Autor:</label>
-                        <input placeholder='Robert C. Martin' type="text" id="autor" name="autor" required />
+                        <input
+                            placeholder='Robert C. Martin'
+                            name="autores"
+                            value={libro.autores}
+                            onChange={handleChange}
+                            type="text"
+                            id="autor"
+                            required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="publicación">Año de publicación:</label>
-                        <input placeholder='2005' type="number" id="publicacion" name="publicacion" required />
+                        <input placeholder='2005'
+                            name="anioPublicacion"
+                            value={libro.anioPublicacion}
+                            onChange={handleChange}
+                            type="number"
+                            id="publicacion"
+                            required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="paginas">Cantidad de páginas:</label>
-                        <input placeholder='464' type="number" id="paginas" name="paginas" required />
+                        <input placeholder='464'
+                            value={libro.paginas}
+                            onChange={handleChange}
+                            type="number"
+                            id="paginas"
+                            name="paginas"
+                            required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="idioma">Idioma:</label>
-                        <input placeholder='Ingles' type="text" id="idioma" name="idioma" required />
+                        <input placeholder='Ingles'
+                            value={libro.idioma}
+                            onChange={handleChange}
+                            type="text"
+                            id="idioma"
+                            name="idioma"
+                            required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="libro">Link del libro:</label>
-                        <input type="text" id="libro" name="libro" required />
+                        <input type="text"
+                            value={libro.linkPdf}
+                            onChange={handleChange}
+                            id="libro"
+                            name="linkPdf"
+                            required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="descripcion">Descripcion:</label>
-                        <textarea rows="4" placeholder='Libro que se enfoca en el desarrollo de software que prioriza la legibilidad, claridad y mantenibilidad del código' type="text" id="descripcion" name="descripcion" required />
+                        <textarea
+                            rows="4"
+                            value={libro.descripcion}
+                            onChange={handleChange}
+                            id="descripcion"
+                            name="descripcion"
+                            required
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="portada">Link de la portada:</label>
@@ -49,8 +148,16 @@ function CargarLibroPage() {
                             id="portada"
                             name="portada"
                             placeholder="https://..."
-                            value={portadaUrl}
-                            onChange={(e) => setPortadaUrl(e.target.value)}
+                            value={libro.portada}
+                            onChange={(e) => {
+                                setPortadaUrl(e.target.value);
+
+                                setLibro({
+                                    ...libro,
+                                    portada: e.target.value
+                                });
+                            }}
+
                         />
                         {portadaUrl && (
                             <div className="portada-preview">
